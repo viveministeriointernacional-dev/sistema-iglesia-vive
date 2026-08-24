4e608639a9458e4ac5412115a684ea67c885d425
Santiago Viveros <santiagoviveros18@gmail.com>
2026-08-23 22:52:12 -0500

    chore: hacer idempotente migración de asistencia

--- prisma/migrations/20260824010000_asistencia_iglesia/migration.sql
+++ prisma/migrations/20260824010000_asistencia_iglesia/migration.sql
@@ -1,9 +1,15 @@
-CREATE TYPE "ChurchAttendance" AS ENUM (
-  'IGLESIA_VIVE',
-  'OTRA_IGLESIA',
-  'NUEVO',
-  'ASISTIA_ANTES'
-);
+DO $$
+BEGIN
+  CREATE TYPE "ChurchAttendance" AS ENUM (
+    'IGLESIA_VIVE',
+    'OTRA_IGLESIA',
+    'NUEVO',
+    'ASISTIA_ANTES'
+  );
+EXCEPTION
+  WHEN duplicate_object THEN NULL;
+END
+$$;
 
 ALTER TABLE "learner_profile"
-ADD COLUMN "church_attendance" "ChurchAttendance";
+ADD COLUMN IF NOT EXISTS "church_attendance" "ChurchAttendance";
